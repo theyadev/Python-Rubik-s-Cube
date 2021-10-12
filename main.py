66fcc1d55be57641ef8305b311cde2d30d934bde
theyadev <gtheya@gmail.com>
2021-10-12 16:11:40 +0200

    Clear bottom of the console

--- main.py
+++ main.py
@@ -12,5 +12,8 @@ if __name__ == "__main__":
     printCube()
 
     while True:
+        print('\n\033[K'*10)
+        print("\033[10A")
         algo = input('\nQue voulez vous tourner ? \033[K').upper().replace(' ', "")
+
         moveCube(algo, 0.2)
